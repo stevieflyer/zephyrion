53583a080a897bc0ca881e7deb09e03678097e92
stevieflyer <steveflyer7@gmail.com>
2023-08-22 15:43:20 +0800

    debug the logging module

--- zephyrion/pypp/js_util/js_handler/action_handler/_scroll_handler.py
+++ zephyrion/pypp/js_util/js_handler/action_handler/_scroll_handler.py
@@ -151,8 +151,7 @@ class ScrollHandler(JsHandler):
             self.debug_tool.debug(f'Scroll top: {top}, last top: {last_top}')
             if top == last_top:
                 same_count += 1
-                if same_count % 10 == 0:
-                    self.debug_tool.info(f'Top unchanged, Scroll top: {top}, last top: {last_top}, same count: {same_count}, same_th: {same_th}')
+                self.debug_tool.info(f'Top unchanged, Scroll top: {top}, last top: {last_top}, same count: {same_count}, same_th: {same_th}')
                 if same_count >= same_th:
                     break
             else:
