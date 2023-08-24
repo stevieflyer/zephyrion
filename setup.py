85b6ea538943710998ffa0e302a7317ac81e9842
stevieflyer <steveflyer7@gmail.com>
2023-08-24 15:29:29 +0800

    update version to 0.1.12

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_requirements():
 
 setup(
     name='zephyrion',
-    version='0.1.11',
+    version='0.1.12',
     packages=find_packages(exclude=['test', 'test.*']),
     install_requires=read_requirements(),
     url='https://github.com/stevieflyer/zephyrion',
